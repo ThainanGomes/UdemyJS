10dc9611dff619683cbcf1a313b16e4e7a09fdcd
thainan <thainangomez@gmail.com>
2021-07-05 23:19:26 -0400

    Iniciando exercicios em JS

---
+++ Udemy/funcao/Exercicios/02/02.js
@@ -0,0 +1,6 @@
+function funcaoBotao(){
+    var nome = window.prompt("Qual o seu nome?")
+    var idade = window.prompt(`Olá, ${nome}! a sua idade?`)
+
+    alert(`Fico feliz em saber que você tem ${idade} anos, ${nome}`)
+}
---
+++ Udemy/funcao/Exercicios/03/03.js
@@ -0,0 +1,8 @@
+function funcaoBotao(){
+    var num = window.prompt("Digite um número")
+    var ant = num - 1
+    var suc = parseInt(num) + parseInt(1)
+
+    alert(`Antes de ${num} vem o ${ant} e depois, vem o ${suc}`)
+
+}
